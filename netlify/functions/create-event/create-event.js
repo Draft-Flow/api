3e0f9e367eb5fb62177d3f6792bb315f1effcfdb
Brett DeWoody <brettdewoody@gmail.com>
2024-06-18 22:48:58 +0100

    Update create-event

--- netlify/functions/create-event/create-event.js
+++ netlify/functions/create-event/create-event.js
@@ -1,7 +1,54 @@
+import fs from 'fs'
+import {google} from 'googleapis'
+
 export const handler = async (event, context) => {
   const eventBody = JSON.parse(event.body)
-  console.log(eventBody)
+  const {title, _id: id, dates, content} = eventBody
+
+  const privatekey = JSON.parse(process.env.GOOGLE_KEY_FILE)
+  const jwtClient = new google.auth.JWT(
+    privatekey.client_email,
+    null,
+    privatekey.private_key,
+    ['https://www.googleapis.com/auth/calendar']
+  )
   
+  jwtClient.authorize(function (err, tokens) {
+    if (err) {
+      console.log({err});
+      return
+    } else {
+      console.log("Successfully connected!");
+    }
+    }
+  )
+
+  const calendar = google.calendar('v3')
+
+  const calendarCourses = await new Promise((resolve, reject) => {
+    calendar.events.list({
+      auth: jwtClient,
+      calendarId: process.env.GOOGLE_CAL_ID_COURSES,
+      timeMin: formatISO(new Date()),
+      timeMax: formatISO(addMonths(new Date(), 12)),
+      showDeleted: false,
+      maxResults: 200,
+      singleEvents: true,
+      orderBy: 'startTime',
+    }, async (err, res) => {
+      if (err) {
+        console.log('The API returned an error: ' + err)
+        reject('The API returned an error: ' + err)
+        return
+      }
+
+      const coursesJSON = res.data.items
+      resolve(coursesJSON)
+    })
+  })
+
+  console.log({calendarCourses})
+
   return {
     statusCode: 200,
     body: JSON.stringify({
