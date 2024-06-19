2d138dda4c9f644036a53346b8ad465064609d41
Brett DeWoody <brettdewoody@gmail.com>
2024-06-19 22:04:33 +0100

    Add toHTML

--- netlify/functions/create-event/create-event.js
+++ netlify/functions/create-event/create-event.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import {google} from 'googleapis'
 import {formatISO, addMonths} from 'date-fns'
+const { toHTML } = require('@portabletext/to-html')
 
 export const handler = async (event, context) => {
   const eventBody = JSON.parse(event.body)
@@ -59,7 +60,9 @@ export const handler = async (event, context) => {
       calendarId: process.env.GOOGLE_CAL_ID_COURSES,
       resource: {
         summary: title,
-        description: `-- DO NOT EDIT - IT WILL BE OVERWRITTEN -- /n/n/n ${content[0].children.map(child => child.text).join('/n/n')}`,
+        description: `-- DO NOT EDIT - IT WILL BE OVERWRITTEN
+        
+        ${toHTML(content, {components: {}})}`,
         start: {
           dateTime: event.startDate
         },
