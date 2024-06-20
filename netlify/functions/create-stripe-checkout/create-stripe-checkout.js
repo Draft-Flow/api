597bded8210c2e9af62f29993c4155635df060bd
Brett DeWoody <brettdewoody@gmail.com>
2024-06-20 23:22:08 +0100

    Log session

--- netlify/functions/create-stripe-checkout/create-stripe-checkout.js
+++ netlify/functions/create-stripe-checkout/create-stripe-checkout.js
@@ -51,6 +51,8 @@ export const handler = async (event, context, callback) => {
         allowed_countries: ['GB'],
       }
     })
+
+    console.log({session})
   
     return {
       statusCode: 200,
