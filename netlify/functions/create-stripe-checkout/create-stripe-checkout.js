f25539cced13d700d527ac9ab29f21f1bd58b5a1
Brett DeWoody <brettdewoody@gmail.com>
2024-06-20 23:04:13 +0100

    Add Stripe

--- netlify/functions/create-stripe-checkout/create-stripe-checkout.js
+++ netlify/functions/create-stripe-checkout/create-stripe-checkout.js
@@ -1,6 +1,5 @@
-// import Stripe from ('stripe')
-// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-import {google} from 'googleapis'
+import Stripe from ('stripe')
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
@@ -21,47 +20,46 @@ export const handler = async (event, context, callback) => {
 
   const data = JSON.parse(event.body)
   
-  // try {
-  //   const session = await stripe.checkout.sessions.create({
-  //     mode: 'payment',
-  //     line_items: [
-  //       {
-  //         price_data: {
-  //           currency: 'gpb',
-  //           product_data: {
-  //             name: 'Course',
-  //           },
-  //           unit_amount: 2000,
-  //         },
-  //         adjustable_quantity: {
-  //           enabled: true,
-  //           minimum: 1,
-  //           maximum: 10,
-  //         },
-  //         quantity: 1,
-  //       },
-  //     ],
-  //     automatic_tax: {
-  //       enabled: true,
-  //     },
-  //     success_url: `${process.env.URL}/course-confirmed?session_id={CHECKOUT_SESSION_ID}`,
-  //     cancel_url: process.env.URL,
-  //     billing_address_collection: 'required',
-  //     shipping_address_collection: {
-  //       allowed_countries: ['GB'],
-  //     }
-  //   })
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      line_items: [
+        {
+          price_data: {
+            currency: 'gpb',
+            product_data: {
+              name: 'Course',
+            },
+            unit_amount: 2000,
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+            maximum: 10,
+          },
+          quantity: 1,
+        },
+      ],
+      automatic_tax: {
+        enabled: true,
+      },
+      success_url: `${process.env.URL}/course-confirmed?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: process.env.URL,
+      billing_address_collection: 'required',
+      shipping_address_collection: {
+        allowed_countries: ['GB'],
+      }
+    })
   
     return {
       headers: CORS_HEADERS,
       statusCode: 200,
-      // body: JSON.stringify(session)
-      body: JSON.stringify('session')
+      body: JSON.stringify(session)
     }
-  // } catch (error) {
-  //   return {
-  //     statusCode: 400,
-  //     body: error.type,
-  //   }
-  // }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: error.type,
+    }
+  }
 }
