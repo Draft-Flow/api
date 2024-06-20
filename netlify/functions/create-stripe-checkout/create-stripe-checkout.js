5e6c868cdab16307761386909493eedca136f248
Brett DeWoody <brettdewoody@gmail.com>
2024-06-20 15:09:48 +0100

    Handle error

--- netlify/functions/create-stripe-checkout/create-stripe-checkout.js
+++ netlify/functions/create-stripe-checkout/create-stripe-checkout.js
@@ -1,28 +1,35 @@
 export const handler = async function(event, context, callback) {
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
   const data = JSON.parse(event.body)
-
-  const session = await stripe.checkout.sessions.create({
-    mode: 'payment',
-    line_items: [
-      {
-        price: 1000,
-        quantity: 1,
-      },
-    ],
-    automatic_tax: {
-      enabled: true,
-    },
-    success_url: `${process.env.URL}/course-confirmed?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: process.env.URL,
-    billing_address_collection: 'required',
-    shipping_address_collection: {
-      allowed_countries: ['GB'],
-    }
-  })
   
-  return {
-    statusCode: 200,
-    body: JSON.stringify(session)
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      line_items: [
+        {
+          price: 1000,
+          quantity: 1,
+        },
+      ],
+      automatic_tax: {
+        enabled: true,
+      },
+      success_url: `${process.env.URL}/course-confirmed?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: process.env.URL,
+      billing_address_collection: 'required',
+      shipping_address_collection: {
+        allowed_countries: ['GB'],
+      }
+    })
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify(session)
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: error.type,
+    }
   }
 }
