c0871d49ee765b5b2249caf8d7afcbfcff0c5ebd
Brett DeWoody <brettdewoody@gmail.com>
2024-06-21 09:37:41 +0100

    Add logs

--- clients/sanity/sanity.js
+++ clients/sanity/sanity.js
@@ -1,9 +1,10 @@
 const {createClient} = require('@sanity/client')
 
-const { sanity } = require('./config')
+console.log( process.env.SANITY_PROJECT_ID, process.env.SANITY_DATASET)
 
 module.exports = createClient({
-  ...sanity,
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
   useCdn: !process.env.SANITY_READ_TOKEN,
   token: process.env.SANITY_READ_TOKEN,
   apiVersion: '2024-06-19',
