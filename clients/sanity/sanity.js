fbc77a8a1c0cb1cbba7c688eeda4dc5689c52c97
Brett DeWoody <brettdewoody@gmail.com>
2024-06-21 09:30:47 +0100

    Remove log

--- clients/sanity/sanity.js
+++ clients/sanity/sanity.js
@@ -2,8 +2,6 @@ const {createClient} = require('@sanity/client')
 
 const { sanity } = require('./config')
 
-console.log({sanityClient})
-
 module.exports = createClient({
   ...sanity,
   useCdn: !process.env.SANITY_READ_TOKEN,
